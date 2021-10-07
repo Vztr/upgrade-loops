bd121a3e73719d8ae63dbffd01314d6062fc51f8
Victor Zorrilla Brasal <victorzorrillabrasal@gmail.com>
2021-10-07 21:02:14 +0200

    iteration#1

--- upgrade-loops.js
+++ upgrade-loops.js
@@ -1,6 +1,22 @@
+// _________ 1.1
+
 const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
 let newArray = [];
 
 for (let i = 0; i < products.length; i++) {
     newArray = (products[i].includes('camiseta'))
+	return newArray
 }
+console.log(newArray);
+/* 
+// _________ 1.2
+
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+	{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+	{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+	{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+	{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+// _________ 1.3 */
