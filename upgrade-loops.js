090a091db40e78febb5289e06a3bbb6ee1f92d45
Victor Zorrilla Brasal <victorzorrillabrasal@gmail.com>
2021-10-07 20:17:34 +0200

    initialcommit

---
+++ upgrade-loops.js
@@ -0,0 +1,6 @@
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta'];
+let newArray = [];
+
+for (let i = 0; i < products.length; i++) {
+    newArray = (products[i].includes('camiseta'))
+}
